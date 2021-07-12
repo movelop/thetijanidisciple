970b098ad451767a54da5468c6fc15547131d728
Babalola Muhammad <barmilitary@yahoo.com>
2021-07-12 11:19:38 +0100

    quiz fixed

--- src/Quiz-components/Modal.js
+++ src/Quiz-components/Modal.js
@@ -2,28 +2,30 @@ import React from 'react';
 
  const Modal = ({onClose, results, data}) => {
     return (
-        <div className= "modal is-active">
-            <div className ="modal-background" onClick ={onClose}></div>
-            <div className = "modal-card">
-                <header className ="modal-card-head">
-                    <p className = "modal-card-title">Your answers</p>
-                    <button className = "delete" onClick = {onClose}></button>
-                </header>
-                <section className = "modal-card-body content">
-                    <ul>
-                        {results.map((result, i) => (
-                            <li key ={i} className ="mb-6">
-                                <p><strong>{result.q}</strong></p>
-                                <p className = {result.a === data[i].answer ? `has-background-success has-text-white`: 
-                                `has-background-danger has-text-white p-2`}>Your answer:{result.a}</p>
-                                {result.a !== data[i].answer && <p className ="has-background-link has-text-white p-2">Correst answer: {data[i].answer}</p>}
-                                <p className ="has-background-primary has-text-white p-2">Explanation: {data[i].id}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            </div>
-        </div>
+            <div className= "modal is-active mt-50">
+                <div className ="modal-background " onClick ={onClose}></div>
+                    <div className = "modal-card">
+                            <header className ="modal-card-head">
+                                <p className = "modal-card-title">Your answers</p>
+                                <button className = "delete" onClick = {onClose}></button>
+                            </header>
+                            <section className = "modal-card-body content">
+                                <ul>
+                                    {results.map((result, i) => (
+                                    <li key ={i} className ="mb-6">
+                                    <p><strong>{result.q}</strong></p>
+                                    <p className = {result.a === data[i].answer ? `has-background-success has-text-white`: 
+                                    `has-background-danger has-text-white p-2`}>Your answer:{result.a}</p>
+                                    {result.a !== data[i].answer && <p className ="has-background-link has-text-white p-2">Correst answer: {data[i].answer}</p>}
+                                    <p className ="has-background-primary has-text-white p-2">Explanation: {data[i].id}</p>
+                                    </li>
+                                    ))}
+                                </ul>
+                            </section>
+                        </div>    
+                </div>
+            
+
     )
 }
 
